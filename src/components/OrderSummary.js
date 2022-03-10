b6bc30e6523d8718b86d85e3aea7f523cbf32c18
Eleanor Johnson <eleanor.johnson@weareaccess.co.uk>
2022-03-10 11:40:05 +0000

    Commit of intitial set up work, with much of App, Card and OrderSummary tried out.

---
+++ src/components/OrderSummary.js
@@ -0,0 +1,18 @@
+// import OderSummaryForm from './OrderSummaryForm';
+import './OrderSummary.css';
+import header from '../images/illustration-hero.svg';
+
+function OrderSummary (props) {
+
+    return (
+        <div className='order-summary'>
+            <img className='order-summary__header' src={header} />
+            <h2 className='order-summary__title'>Order Summary</h2>
+            <p className='order-summary__text'>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhaere you like!</p>
+
+        </div>
+    );
+
+}
+
+export default OrderSummary;
